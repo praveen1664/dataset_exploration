38f1b44f489aeafa6405d4b34b6c1357e9d5fc1a
Praveen <36715149+praveen1664@users.noreply.github.com>
2021-12-28 22:00:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ echo "\
 email = \"productspraveen@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
+[theme]\n\
+primaryColor="#d33682"\n\
+backgroundColor="#002b36"\n\
+secondaryBackgroundColor="#586e75"\n\
+textColor="#fafafa"\n\
+font="sans serif"\n\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
